c8aa2940c9b9857b60d5f139322fc6d854ba4d8f
Zainurosyid-creator <rositmuhammad8@gmail.com>
2026-08-06 13:50:41 +0700

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,28 @@
+const CACHE_NAME = "ekgjg-v1";
+
+const FILES = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json"
+];
+
 self.addEventListener("install", (event) => {
   console.log("Service Worker terpasang");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

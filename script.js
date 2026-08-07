5bda06fcfce74205a1dc5b210324fe733120f261
Zainurosyid-creator <rositmuhammad8@gmail.com>
2026-08-07 16:25:20 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
-      if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("sw.js");
-      }
-      function toggleMenu() {
-      document.getElementById("menu").classList.toggle("show");
-      }
-      fetch('https://ipwho.is/')
-      .then(res => res.json())
-      .then(data => {
-      document.getElementById("ip").innerText = "IP: " + data.ip;
-      document.getElementById("isp").innerText = "ISP: " + data.connection.isp;
-      })
-      .catch(err => {
-      document.getElementById("ip").innerText = "Error: " + err;
-      document.getElementById("isp").innerText = "Gagal memuat";
-    console.error(err);
-      });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("show");
+}
+fetch('https://ipwho.is/')
+.then(res => res.json())
+.then(data => {
+  document.getElementById("ip").innerText = "IP: " + data.ip;
+  document.getElementById("isp").innerText = "ISP: " + data.connection.isp;
+})
+.catch(err => {
+  document.getElementById("ip").innerText = "Error: " + err;
+  document.getElementById("isp").innerText = "Gagal memuat";
+  console.error(err);
+});
